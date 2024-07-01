d7fb761491ee0320d3af76e5117268a3a1587d57
kevin <kevinancel@gmail.com>
2024-07-01 22:23:36 +0200

    Initial Commit

---
+++ FlotteManager/FlotteManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlotteManagerApp.swift
+//  FlotteManager
+//
+//  Created by Ancel Kevin on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlotteManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
